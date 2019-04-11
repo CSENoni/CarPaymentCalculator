e4bd828d8b025bf245ad7efe1e179f6587e29d4e
CSENoni <nonivhm@gmail.com>
2019-04-11 15:49:19 -0500

    Add Calculation for monthly Payment (not correct and need to improve)

--- src/application/FinanceCalculatorModel.java
+++ src/application/FinanceCalculatorModel.java
@@ -1,5 +1,19 @@
 package application;
 
 public class FinanceCalculatorModel {
+	// TODO: fill out these functions here and add more or less arguments if needed
+	
+	public double getMonthlyPayment(double carPrice, double downPayment, double interest, double numberOfMonths) {
+		// not a full and correct calculation, just give first calculation to indicate the function works
+		// need a full formula to calculate this one
+		return carPrice * numberOfMonths;
+	}
 
+	public double getTotalAmountPaid() {
+		return 0.0;
+	}
+
+	public double getTotalInterestPaid() {
+		return 0.0;
+	}
 }
--- src/application/InputController.java
+++ src/application/InputController.java
@@ -13,6 +13,8 @@ public class InputController {
 	
 	private OutputController outputController;
 	
+	private FinanceCalculatorModel model;
+	
 	@FXML
 	private ComboBox<String> userCredit;
 
@@ -26,26 +28,33 @@ public class InputController {
 	private TextField userMonths;
 	
 	@FXML
+	private TextField userDown;
+	
+	@FXML
+	private TextField userInterest;
+	
+	@FXML
 	public void initialize() {
+		this.model = new FinanceCalculatorModel();
 		userCredit.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 				switch (newValue) {
 				case "Super prime (781-850)":
-					creditExplain.setText("Based on your score, the average rate is 3.68% (new) or 4.34% (used).");
+					creditExplain.setText("Based on your score, the average rate is 3.08% (new) or 3.76% (used).");
 					break;
 				case "Prime (661-780)":
-					creditExplain.setText("Based on your score, the average rate is 4.56% (new) or 5.97% (used).");
+					creditExplain.setText("Based on your score, the average rate is 3.99% (new) or 5.45% (used).");
 					break;
 				case "Subprime (501-600)":
-					creditExplain.setText("Based on your score, the average rate is 11.89% (new) or 16.14% (used).");
+					creditExplain.setText("Based on your score, the average rate is 11.11% (new) or 16.23% (used).");
 					break;
 				case "Nonprime (601-660)":
-					creditExplain.setText("Based on your score, the average rate is 7.52% (new) or 10.34% (used).");
+					creditExplain.setText("Based on your score, the average rate is 6.83% (new) or 9.98% (used).");
 					break;
 				default:
 					creditExplain.setText(
-							"Based on your score, the average rate is 14.41% (new) or 19.98% (used). Consider buying an inexpensive used car and refinancing in 6-12 months.");
+							"Based on your score, the average rate is 13.95% (new) or 19.38% (used). Consider buying an inexpensive used car and refinancing in 6-12 months.");
 				}
 			}
 		});
@@ -57,10 +66,34 @@ public class InputController {
 	
 	@FXML
 	public void handleCarPriceAction(KeyEvent event) {
-		// May change based on calculation
-		outputController.setOutputMonthly(userCarPrice.getText());
+		calculateAndShowMonthlyPayment();
 	}
 	
+	@FXML
+	public void handleMonthAction(KeyEvent event) {
+		calculateAndShowMonthlyPayment();
+	}
 	// TODO: Write more key handler in fxml and this inputcontroller like above function
 	// May need to declare more data fields at the top with @FXML...
+	
+	
+	
+	
+	
+	/******************Helper Functions************************/
+	// Check to see if the string is empty or not
+	private boolean isNotEmpty(String text) {
+		return text != null && text.length() > 0;
+	}
+	
+	// Calculate monthly payment. TODO: Improve the calculation
+	private void calculateAndShowMonthlyPayment() {
+		double price = (isNotEmpty(userCarPrice.getText())) ? Double.parseDouble(userCarPrice.getText()) : 0.0;
+		double months = (isNotEmpty(userMonths.getText())) ? Double.parseDouble(userMonths.getText()) : 0.0;
+		double downPayment = (isNotEmpty(userDown.getText())) ? Double.parseDouble(userDown.getText()) : 0.0;
+		double interest = (isNotEmpty(userInterest.getText())) ? Double.parseDouble(userInterest.getText()) : 0.0;
+		
+		String monthlyPayment = "$" + model.getMonthlyPayment(price, downPayment, interest, months);
+		outputController.setOutputMonthly(monthlyPayment);
+	}
 }
